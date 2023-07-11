3d1bc8fd6995800297bca0f714b675b0cb005f92
Ribbit <echo07200503@gmail.com>
2023-07-11 10:17:03 +0900

    ✨ チャット履歴の削除機能

--- src/desktop/original-view/components/model/sidebar/histories.tsx
+++ src/desktop/original-view/components/model/sidebar/histories.tsx
@@ -2,9 +2,12 @@ import {
   apiErrorMessageState,
   chatHistoriesState,
   historiesFetchedState,
+  loadingState,
+  pluginConfigState,
   selectedHistoryIdState,
 } from '@/desktop/original-view/states/states';
 import {
+  IconButton,
   List,
   ListItem,
   ListItemButton,
@@ -14,19 +17,58 @@ import {
 } from '@mui/material';
 import React, { FCX } from 'react';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ChatIcon from '@mui/icons-material/Chat';
+import DeleteIcon from '@mui/icons-material/Delete';
 import styled from '@emotion/styled';
+import { deleteAllRecords, isGuestSpace, withSpaceIdFallback } from '@konomi-app/kintone-utilities';
+import { produce } from 'immer';
+import { useSnackbar } from 'notistack';
 
 const Component: FCX = ({ className }) => {
   const historiesFetched = useRecoilValue(historiesFetchedState);
   const histories = useRecoilValue(chatHistoriesState);
   const selectedHistoryId = useRecoilValue(selectedHistoryIdState);
+  const loading = useRecoilValue(loadingState);
+  const { enqueueSnackbar } = useSnackbar();
 
   const onHistoryChange = useRecoilCallback(
     ({ reset, set }) =>
       (historyId: string) => {
         set(selectedHistoryIdState, historyId);
         reset(apiErrorMessageState);
+      },
+    []
+  );
+
+  const onDeleteButtonClick = useRecoilCallback(
+    ({ reset, set, snapshot }) =>
+      async () => {
+        try {
+          set(loadingState, true);
+          const id = (await snapshot.getPromise(selectedHistoryIdState))!;
+          const { outputAppId, outputAppSpaceId } = (await snapshot.getPromise(pluginConfigState))!;
+
+          const isGuest = await isGuestSpace(outputAppId);
+
+          await deleteAllRecords({
+            app: outputAppId,
+            ids: [Number(id)],
+            debug: process.env.NODE_ENV === 'development',
+            guestSpaceId: isGuest ? outputAppSpaceId : undefined,
+          });
+
+          set(chatHistoriesState, (_histories) =>
+            produce(_histories, (draft) => {
+              const index = draft.findIndex((history) => history.id === id);
+              draft.splice(index, 1);
+            })
+          );
+
+          reset(selectedHistoryIdState);
+          enqueueSnackbar('履歴を削除しました', { variant: 'success' });
+        } finally {
+          reset(loadingState);
+        }
       },
     []
   );
@@ -39,7 +81,7 @@ const Component: FCX = ({ className }) => {
             <ListItem key={i} disablePadding>
               <ListItemButton>
                 <ListItemIcon>
-                  <ChatBubbleOutlineOutlinedIcon />
+                  <ChatIcon />
                 </ListItemIcon>
                 <ListItemText>
                   <Skeleton />
@@ -60,13 +102,30 @@ const Component: FCX = ({ className }) => {
             key={index}
             onClick={() => onHistoryChange(history.id)}
             disablePadding
-            sx={{ backgroundColor: selectedHistoryId === history.id ? '#1976d222' : undefined }}
+            sx={{
+              backgroundColor: selectedHistoryId === history.id ? '#1976d222' : undefined,
+              position: 'relative',
+            }}
           >
             <ListItemButton>
               <ListItemIcon>
-                <ChatBubbleOutlineOutlinedIcon />
+                <ChatIcon />
               </ListItemIcon>
               <ListItemText primary={history.title} />
+              {selectedHistoryId === history.id && (
+                <IconButton
+                  onClick={onDeleteButtonClick}
+                  disabled={loading}
+                  sx={{
+                    position: 'absolute',
+                    right: '8px',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                  }}
+                >
+                  <DeleteIcon fontSize='small' />
+                </IconButton>
+              )}
             </ListItemButton>
           </ListItem>
         ))}
