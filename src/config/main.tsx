be47e53603ad1e60a2901cac146fe93ec1a187cd
Ribbit <echo07200503@gmail.com>
2023-10-17 09:06:37 +0900

    ♻ 記法を統一

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app';
 
-const main = (): void => {
-  const root = document.getElementById('settings');
-  if (!root) {
-    throw new Error('プラグインのHTMLに、ルート要素が存在しません。');
-  }
-  createRoot(root).render(<App />);
-};
-
-export default main;
+const root = document.getElementById('settings');
+if (!root) {
+  throw new Error('プラグインのHTMLに、ルート要素が存在しません。');
+}
+createRoot(root).render(<App />);
