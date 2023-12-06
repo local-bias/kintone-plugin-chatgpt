21e9a6435205139aab1f796958bf0abe698b0027
Ribbit <echo07200503@gmail.com>
2023-12-06 13:33:51 +0900

    🔥 Remove unused constants and URLs

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -3,20 +3,13 @@ import manifest from '../../plugin/manifest.json';
 export const OPENAI_ENDPOINT_ROOT = 'https://api.openai.com';
 export const OPENAI_ENDPOINT = `${OPENAI_ENDPOINT_ROOT}/v1/chat/completions`;
 
-export const PLUGIN_KEY = 'chatgpt';
 export const PLUGIN_NAME = manifest.name.ja;
-export const PLUGIN_VERSION = manifest.version;
-
-export const LOCAL_STORAGE_PREFIX = 'ribbit-kintone-plugin-';
-export const LOCAL_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}${PLUGIN_KEY}`;
 
 export const VIEW_ROOT_ID = `ribbit-chatgpt-plugin-root`;
 
-export const URL_HOMEPAGE = manifest.homepage_url.ja;
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
-export const URL_PLUGIN_LIST = 'https://ribbit.konomi.app/kintone-plugin';
 
 export const OPENAI_MODELS = ['gpt-3.5-turbo', 'gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-1106-preview'];
 
