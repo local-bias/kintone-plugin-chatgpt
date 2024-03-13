6646dbd96eebe2bc3d9926b146e48b3e66106dcc
Ribbit <echo07200503@gmail.com>
2024-03-13 10:45:38 +0900

    🚀 4.14.0

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -1,16 +1,17 @@
 //@ts-check
-const hp = 'https://konomi.app/';
-const commonCdn = 'https://kintone-plugin.konomi.app/common';
+const hp = 'https://konomi.app';
+const cdn = 'https://kintone-plugin.konomi.app';
+const key = 'chatgpt';
 const localhost = 'https://127.0.0.1:9152';
 
-/** @type {import('@konomi-app/kintone-utilities').PluginConfig} */
+/** @type { import('@konomi-app/kintone-utilities').PluginConfig } */
 export default {
-  id: 'ribbit-kintone-plugin-chatgpt',
+  id: `ribbit-kintone-plugin-${key}`,
   pluginReleasePageUrl: 'https://ribbit.konomi.app/kintone-plugin/',
   manifest: {
     base: {
       manifest_version: 1,
-      version: '4.13.0',
+      version: '4.14.0',
       type: 'APP',
       name: {
         en: 'ChatGPT integration plugin',
@@ -24,39 +25,33 @@ export default {
       },
       icon: 'icon.png',
       homepage_url: { ja: hp, en: hp },
-      desktop: {
-        js: [`${commonCdn}/desktop.js`],
-        css: [`${commonCdn}/desktop.css`],
-      },
-      mobile: {
-        js: [`${commonCdn}/desktop.js`],
-        css: [`${commonCdn}/desktop.css`],
-      },
+      desktop: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+      mobile: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
       config: {
         html: 'config.html',
-        js: [`${commonCdn}/config.js`],
-        css: [`${commonCdn}/config.css`],
+        js: [`${cdn}/common/config.js`],
+        css: [`${cdn}/common/config.css`],
         required_params: [],
       },
     },
     dev: {
       desktop: {
-        js: [`${localhost}/dist/dev/desktop/index.js`],
+        js: [`${localhost}/dist/dev/desktop.js`],
         css: [`${localhost}/dist/dev/desktop.css`],
       },
       mobile: {
-        js: [`${localhost}/dist/dev/desktop/index.js`],
+        js: [`${localhost}/dist/dev/desktop.js`],
         css: [`${localhost}/dist/dev/desktop.css`],
       },
       config: {
-        js: [`${localhost}/dist/dev/config/index.js`],
+        js: [`${localhost}/dist/dev/config.js`],
         css: [`${localhost}/dist/dev/config.css`],
       },
     },
     prod: {
-      desktop: { js: [`desktop.js`], css: [`desktop.css`] },
-      mobile: { js: [`desktop.js`], css: [`desktop.css`] },
-      config: { js: [`config.js`], css: [`config.css`] },
+      desktop: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+      mobile: { js: [`${cdn}/${key}/desktop.js`], css: [`${cdn}/${key}/desktop.css`] },
+      config: { js: [`${cdn}/${key}/config.js`], css: [`${cdn}/${key}/config.css`] },
     },
     standalone: {
       desktop: { js: ['desktop.js'], css: ['desktop.css'] },
