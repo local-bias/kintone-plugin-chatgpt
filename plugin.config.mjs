23ccc985e6ed92488d666610aab74b4e2b757c1f
Ribbit <echo07200503@gmail.com>
2024-05-14 12:27:05 +0900

    🚀 4.16.0

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -11,7 +11,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '4.15.0',
+      version: '4.16.0',
       type: 'APP',
       name: {
         en: 'ChatGPT integration plugin',
